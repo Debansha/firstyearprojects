2bf0026b29929f4519aae83805a9af1c285ecddc
Debansha Datta <debanshadatta20@gmail.com>
2025-05-25 19:09:24 +0530

    Add files via upload

---
+++ Python/happynewyearaudiowish.py
@@ -0,0 +1,22 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say(
+    "Happy New Year 2025! "
+    "Wishing you a fantastic year ahead!"
+)
+
+print("Happy New Year 2025! Wishing you a fantastic year ahead!")
+
+engine.runAndWait()
+
+
+for _ in range(1):
+    print("✨        ✨         ✨")
+    print("     ✨        ✨        ")
+    print("✨        ✨         ✨")
+    print("🎉 Happy New Year! 2025 🎉\n")
+    print("✨        ✨         ✨")
+    print("     ✨        ✨        ")
+    print("✨        ✨         ✨")
